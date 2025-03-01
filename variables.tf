93dca329f1231622f3185fd89e4725cefe46377c
Puranmishra09 <109064029+Puranmishra09@users.noreply.github.com>
2025-03-01 20:59:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -49,11 +49,6 @@ variable "alb_target_protocol" {
 variable "asg_name" {
   default = "my-auto-scaling-group"
 }
-
-resource "aws_autoscaling_group" "web_asg" {
-  name = var.asg_name
-}
-
 variable "scale_up_threshold" {
   description = "CPU utilization percentage for scale-up"
   type        = number
