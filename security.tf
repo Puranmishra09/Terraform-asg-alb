6ad7c2eced9e535cfb8165e3cd30ec503339448a
Puranmishra09 <109064029+Puranmishra09@users.noreply.github.com>
2025-03-01 19:34:41 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,38 @@
+## Allows traffic to and from the ALB and EC2 instances
+resource "aws_security_group" "alb_sg" {
+  name_prefix = "alb-sg-"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "asg_sg" {
+  name_prefix = "asg-sg-"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
